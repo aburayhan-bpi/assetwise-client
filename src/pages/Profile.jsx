92db46bef4f70cdfd84df05ea6638cc842508a41
aburayhan-bpi <aburayhan.bpi@gmail.com>
2025-01-26 11:50:29 +0600

    user name update feature implemented (firebase, mongodb db both)

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useAuth from "../hooks/useAuth";
 import useCurrentUser from "../hooks/useCurrentUser";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import toast from "react-hot-toast";
 
 const Profile = () => {
-  const [name, setName] = useState("");
-  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { user, updateUserProfile } = useAuth();
   const currentUser = useCurrentUser();
-  console.log(name);
+  console.log(currentUser);
+  const [name, setName] = useState("");
+  useEffect(() => {
+    if (currentUser?.name) {
+      setName(currentUser.name);
+    }
+  }, [currentUser]);
+  // handle name change
+  const handleNameChange = async (empId) => {
+    // update name in firebase
+    const currentPhotoURL = user?.photoURL;
+    // console.log(name, currentPhotoURL);
+    await updateUserProfile(name, currentPhotoURL).then((result) => {
+      console.log(result);
+    });
+
+    // update name to database also
+    axiosSecure.patch(`/update-name/${empId}?newName=${name}`).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount > 0) {
+        toast.success("Name updated!");
+      }
+    });
+
+    console.log(empId);
+  };
+
   return (
     <div className="max-w-2xl mx-auto mt-20 p-6 bg-white shadow-lg rounded-lg">
       <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
@@ -38,7 +66,10 @@ const Profile = () => {
 
       {/* Buttons */}
       <div className="flex justify-end gap-4">
-        <button className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-all duration-200">
+        <button
+          onClick={() => handleNameChange(currentUser?._id)}
+          className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition-all duration-200"
+        >
           Update
         </button>
         <button className="bg-gray-300 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-400 transition-all duration-200">
